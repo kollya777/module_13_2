f211c5145ab94106c9587ae3e99e74e6c93d747d
kollya777 <kollya777@mail.ru>
2024-09-19 10:44:52 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+
+api = '7393557259:AAG41hFgRvtTdzV9EfpbgxfW1pC02Xti0O4'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+# Запускается только тогда, когда написана команда '/start' в чате с ботом
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print("Привет! Я бот, помогающий твоему здоровью")
+
+
+# Запускается при любом обращении. Не описаном ранее.
+@dp.message_handler()
+async def all_message(message):
+    print("Введите команду /start, чтобы начать общение")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
